d1899bf111b07e7b80e8b53154a60c7bdd507fd2
florencia.di.ciocco <florencia.di.ciocco@gmail.com>
2021-02-22 23:35:30 -0300

    finished

--- java.js
+++ java.js
@@ -5,14 +5,24 @@ window.addEventListener('scroll', function() {
 
 	// checking whether fully visible
 	if(position.top >= 0 && position.bottom <= window.innerHeight) {
-        document.getElementById("my_name").style.visibility = "hidden";
+        document.getElementById("my_name").style.display = "none";
 	}
 	// checking for partial visibility
 	else if(position.top < window.innerHeight && position.bottom >= 0) {
-        document.getElementById("my_name").style.visibility = "hidden";
+        document.getElementById("my_name").style.display = "inline";
 	}
     // checking for invisibility
     else{
-        document.getElementById("my_name").style.visibility = "visible";
+        document.getElementById("my_name").style.display = "inline";
     }
 });
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }
